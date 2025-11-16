bab503510e9df9d57623ae7c52fc4b681ddd325a
hawaiidev <hawaiilive@outlook.com>
2025-11-16 17:15:32 +0100

    clean slate

--- client/src/components/answer/components/type.ts
+++
@@ -1,9 +0,0 @@
-import { SmallGame } from "@/types/game";
-
-export type UpdateAnswer = (
-  type: SmallGame["type"],
-  key: string,
-  val: string,
-) => void;
-
-export type GetAnswer = (type: SmallGame["type"], key: string) => string;
--- client/src/types/player.ts
+++
@@ -1,5 +0,0 @@
-export type LobbyPlayer = {
-  id: string;
-  name: string;
-  isHost: boolean;
-};
